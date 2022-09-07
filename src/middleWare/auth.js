57d353d1e2562349ff4ce5fb40863c430798f025
FirozSkAhmad <you@example.com>
2022-09-07 17:38:58 +0530

    upD

--- src/middleWare/auth.js
+++ src/middleWare/auth.js
@@ -1,27 +1,26 @@
 const authorModel = require("../model/authorModel");
 const blogModel = require("../model/blogModel");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 const Authentication = async function (req, res, next) {
-    try {
-        const getToken = req.headers["x-auth-token"]
-        console.log(getToken);
-        if (!getToken) {
-            return res.status(401).send({ staus: false, msg: "require token " })
-        }
-
-         req.decodePaylode = jwt.verify(getToken, "project-pltm")
-        console.log(decodePaylode);
-        if (!req.decodePaylode) {
-            return res.status(401).send({ status: false, msg: "Invalid token" })
-        }
-        next()
-
-    } catch (error) {
-        console.log(error.message)
-        return res.status(500).send({ staus: false, msg: error.message })
-
+  try {
+    const getToken = req.headers["x-auth-token"];
+    if (!getToken) {
+      return res.status(401).send({ staus: false, msg: "require token " });
     }
-}
+    jwt.verify(getToken, "project-pltm", (error,decoded) => {
+      if (error) {
+        return res.status(401).send({ status: false, msg: error.message });
+      } else {
+        req.decodedPayload = decoded;
+        console.log(req.decodedPayload)
+        next();
+      }
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({ staus: false, msg: error.message });
+  }
+};
 
-module.exports.Authentication = Authentication
+module.exports.Authentication = Authentication;
